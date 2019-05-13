8b7b422eddd598c2f54c3480bec9518269e50c78
Tejas <tejaskatakdhond@gmail.com>
2019-05-13 22:33:40 +0530

    - partial implementation of event handlers for button click and updating value in the textbox

--- Calculator.py
+++ Calculator.py
@@ -1,12 +1,24 @@
 import tkinter
+from tkinter import *
 
 c = tkinter.Tk()
 c.title('python calculator');
 	
+	
+# Event listners
+def btn_1_click():
+	soltext = "dd"
+	print(soltext)
+
+# Member Variable
+soltext = tkinter.StringVar()
+soltext = 's'
+	
 # UI creation
-tkinter.Entry(c).grid(row=0, columnspan=4, sticky= tkinter.W + tkinter.E)
+textWindow = tkinter.Entry(c, textvariable=soltext)
+textWindow.grid(row=0, columnspan=4, sticky= tkinter.W + tkinter.E)
 tkinter.Label(c, text="welcome ...").grid(row=1, columnspan=4, sticky= tkinter.W + tkinter.E)
-tkinter.Button(c, text='1', width=5, height=2).grid(row=2, column=0)
+tkinter.Button(c, text='1', width=5, height=2, command=btn_1_click).grid(row=2, column=0)
 tkinter.Button(c, text='2', width=5, height=2).grid(row=2, column=1)
 tkinter.Button(c, text='3', width=5, height=2).grid(row=2, column=2)
 tkinter.Button(c, text='+', width=5, height=2).grid(row=2, column=3)
@@ -22,6 +34,7 @@ tkinter.Button(c, text='.', width=5, height=2).grid(row=5, column=0)
 tkinter.Button(c, text='0', width=5, height=2).grid(row=5, column=1)
 tkinter.Button(c, text='±', width=5, height=2).grid(row=5, column=2)
 tkinter.Button(c, text='*', width=5, height=2).grid(row=5, column=3)
+textWindow.focus()
+c.bind('<Return>', btn_1_click)
 c.mainloop()
-
 
